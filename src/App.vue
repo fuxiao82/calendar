f8aa171312751459438ffee00626f0f69f5a5f8a
fuxiao82 <2811773791@qq.com>
2019-04-08 22:10:20 +0800

    for save

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,20 @@
 <template>
   <div id="app">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ul>
+      <li>
+        <a href="#">1</a>
+      </li>
+      <li>
+        <a href="#" class="active">2</a>
+      </li>
+      <li>
+        <a href="#">3</a>
+      </li>
+      <li>
+        <a href="#">4</a>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -20,7 +34,7 @@ html {
   font-size: 100px;
 }
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -31,5 +45,9 @@ html,
 body,
 #app {
   height: 100%;
+}
+
+li:has(> a.active) {
+  color: red;
 }
 </style>
